789c713c106bb144e73002f0a4ec9849247a1659
Geoff Hayes <hayesgm@gmail.com>
2014-01-17 00:37:27 -0800

    another typo

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,7 +20,7 @@ node[:crontasks].each do |application, tasks|
       command %Q{
         cd #{props[:wd]||app_root} &&
         props[:cmd]
-      }.gsub("{app_root}",app_root).gsub("{shared_root}",shared_root).gsub('{env}',env).gsub("\n"," ")
+      }.gsub("{app_root}",app_root).gsub("{shared_root}",shared_root).gsub('{rails_env}',rails_env).gsub("\n"," ")
 
       only_if do File.exists?(app_root) && File.exists?(shared_root) end
       action props[:action] || :create # to remove old cron tasks
